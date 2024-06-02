7b74cc62a4bb7eba5f995cc6b33f870c1774398b
some-molasses <river.stanley173@gmail.com>
2024-06-02 13:30:52 -0400

    Update screen size

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   WIDTH: 800,
-  HEIGHT: 600,
+  HEIGHT: 500,
 
   inWidth: (n: number, padding: number = 0) => {
     return Math.max(Math.min(n, CONFIG.WIDTH - padding), padding);
