dec07aaff31c95522cd22b50da646bfea88f9177
River Stanley <river.stanley173@gmail.com>
2024-06-02 10:18:51 -0400

    Assign players to teams

--- game/entities/player.ts
+++ game/entities/player.ts
@@ -1,9 +1,11 @@
 import { toDecimals } from "../../util";
+import { Team } from "../globals";
 import { Circle } from "./circle";
 
 export class Player extends Circle {
   dx: number;
   dy: number;
+  team: Team;
 
   toJSON() {
     return [
@@ -11,6 +13,7 @@ export class Player extends Circle {
       toDecimals(this.y, 2),
       toDecimals(this.dx, 2),
       toDecimals(this.dy, 2),
+      this.team,
     ];
   }
 }
