8a165e50c6be6c5cee0f9e44a891bfb540c1fcac
River Stanley <river.stanley173@gmail.com>
2024-06-02 09:35:25 -0400

    Update player info sent

--- game/entities/player.ts
+++ game/entities/player.ts
@@ -1,3 +1,16 @@
+import { toDecimals } from "../../util";
 import { Circle } from "./circle";
 
-export class Player extends Circle {}
+export class Player extends Circle {
+  dx: number;
+  dy: number;
+
+  toJSON() {
+    return [
+      toDecimals(this.x, 2),
+      toDecimals(this.y, 2),
+      toDecimals(this.dx, 2),
+      toDecimals(this.dy, 2),
+    ];
+  }
+}
